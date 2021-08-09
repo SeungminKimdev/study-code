2573fd084d9d8083965b68fe2420d82a45f0948f
SeungminKimdev <ksm7250432@gmail.com>
2021-08-09 23:55:36 +0900

    Second

--- Python/webImage.py
+++ Python/webImage.py
@@ -1,29 +1,18 @@
-from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen
-from urllib.parse import quote_plus
-import time
+import urllib.request
+from bs4 import BeautifulSoup
 
-baseUrl = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='
-plusUrl = input('검색어 입력: ') 
-crawl_num = int(input('크롤링할 갯수 입력(최대 50개): '))
+print('Beginning file download with urllib2...')
 
-url = baseUrl + quote_plus(plusUrl) # 한글 검색 자동 변환
-html = urlopen(url)
-soup = bs(html, "html.parser")
-time.sleep(1)
-img = soup.find_all({'class','_image _listImage'})
+url = 'https://search.naver.com/search.naver?where=nexearch&sm=tab_jum&query=%EC%95%84%EC%9D%B4%EC%9C%A0'
+req = urllib.request.Request(url)
+res = urllib.request.urlopen(url).read()
 
-n = 1
-for i in img:
-    print(n)
-    imgUrl = i['data-source']
-    with urlopen(imgUrl) as f:
-        with open('./images/img' + str(n)+'.jpg','wb') as h: # w - write b - binary
-            img = f.read()
-            h.write(img)
-    n += 1
-    if n > crawl_num:
-        break
-    
-    
-print('Image Crawling is done.')
+soup = BeautifulSoup(res,'html.parser')
+print(soup)
+soup = soup.find("div",class_="big_thumb")
+#img의 경로를 받아온다
+imgUrl = soup.find("img")["src"]
+
+#urlretrieve는 다운로드 함수
+#img.alt는 이미지 대체 텍스트 == 마약왕
+urllib.request.urlretrieve(imgUrl, soup.find("img")["alt"]+'.jpg')
