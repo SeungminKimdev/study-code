a5a4d788fc2a57fddc0e2b2010eef5136b38ca3c
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2022-10-01 00:49:33 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,7 +1,4 @@
 chess = [1,1,2,2,2,8]
 inNum = list(map(int,input().split()))
-answer = ''
 for i,j in zip(chess,inNum):
-    answer += str(i-j)
-    answer += ' '
-print(answer[:-1])
+    print(i-j)
