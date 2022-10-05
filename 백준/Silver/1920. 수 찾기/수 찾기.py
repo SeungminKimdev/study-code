12921a0cf8659b522032ee6b248bcf625fa82f5a
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2022-10-05 09:32:05 +0900

    [Silver IV] Title: 수 찾기, Time: 188 ms, Memory: 49760 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,20 +1,9 @@
-def search(tmpList, size, sNum) -> int:
-    minNum = 0
-    middle = size // 2
-    maxNum = size
-    while minNum < maxNum:
-        if tmpList[middle] == sNum:
-            return 1
-        elif tmpList[middle] < sNum:
-            minNum = middle + 1
-            middle = (minNum + maxNum) // 2
-        else:
-            maxNum = middle
-            middle //= 2
-    return 0
 N = int(input())
-ans = sorted(list(map(int,input().split())))
+ans = set(list(map(int,input().split())))
 M = int(input())
 numList = list(map(int,input().split()))
 for i in numList:
-    print(search(ans,N,i))
+    if i in ans:
+        print('1')
+    else:
+        print('0')
