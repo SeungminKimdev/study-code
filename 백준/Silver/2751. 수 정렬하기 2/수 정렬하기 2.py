804d02c98f0c72411595650075e26052044281b4
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-03-24 12:57:27 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4352 ms, Memory: 100124 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr) // 2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
+    
+    merged_arr = []
+    l = h = 0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] < high_arr[h]:
+            merged_arr.append(low_arr[l])
+            l += 1
+        else:
+            merged_arr.append(high_arr[h])
+            h += 1
+    merged_arr += low_arr[l:]
+    merged_arr += high_arr[h:]
+    return merged_arr
+
+array = []
+n = int(input())
+for _ in range(n):
+    array.append(int(input()))
+array = merge_sort(array)
+for i in array:
+    print(i)
