e4f8e7a704aff9af149bbbdf40fbc4a27bab9fe3
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-04-16 14:20:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 3268 ms, Memory: 282468 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,16 +1,21 @@
 import sys
+input = sys.stdin.readline
+
+def knapsack_dp(capacity, n, size, value):
+    memo = [[0 for _ in range(capacity+1)] for _ in range(n+1)]
+    for i in range(1, n+1):
+        for j in range(1, capacity+1):
+            if size[i-1] > j:
+                memo[i][j] = memo[i-1][j]
+            else:
+                memo[i][j] = max(value[i-1] + memo[i-1][j-size[i-1]], memo[i-1][j])
+    return memo[n][capacity]
 
 N, K = map(int, sys.stdin.readline().split()) #N = 물품 수, K = 한계 무게
-items = [] #물품 정보
+size = []
+value = []
 for _ in range(N):
     W, V = map(int, sys.stdin.readline().split()) #W = 무게, V = 가치
-    items.append([W,V])
-
-bag = [[0 for _ in range(N+1)] for _ in range(K+1)] #총 가치를 저장
-for i in range(1,K+1): #무게 증가
-    for j in range(1,N+1): #물품 수 증가
-        bag[i][j] = bag[i][j-1]
-        if(items[j-1][0] <= i):
-            bag[i][j] = max(bag[i][j],bag[i-items[j-1][0]][j-1] + items[j-1][1]) #더 큰 가치를 저장
-            
-print(bag[K][N])
+    size.append(W)
+    value.append(V)
+print(knapsack_dp(K, N, size, value))
