00d7cf1df226fbffc636f40755b82371b77f3898
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-04-16 13:52:07 +0900

    [Silver II] Title: 랜선 자르기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -4,9 +4,20 @@ input = sys.stdin.readline
 def cutting(numList, cut):
     return sum(i//cut for i in numList)
 
+def binary_search_max(numList):
+    low = 0
+    high = len(numList) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if numList[mid] < numList[mid+1]:
+            low = mid + 1
+        else:
+            high = mid
+    return numList[low]
+
 def cutLan(numList, N):
     low = 1
-    high = max(numList)
+    high = binary_search_max(numList)
     ans = 0
     while low <= high:
         mid = (low + high) // 2
