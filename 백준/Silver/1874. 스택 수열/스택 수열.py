939449ed0073f0a6e29fa76b4ed6c0c8eed4afe5
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-04-16 15:22:02 +0900

    [Silver II] Title: 스택 수열, Time: 128 ms, Memory: 37536 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -4,32 +4,26 @@ input = sys.stdin.readline
 stack = []
 n = int(input())
 numList = [int(input()) for i in range(n)]
+
+
 answer = []
 numIn = 1
 for i in numList:
-    if len(stack) == 0:
-        if numIn > i:
-            answer = ['NO']
-            break
-        while numIn != i:
-            stack.append(numIn)
-            answer.append('+')
-            numIn += 1
-        answer += ['+','-']
-        numIn += 1
-    else:
+    if len(stack) == 0 and numIn > i:
+        answer = ['NO']
+        break
+    if len(stack) != 0:
         if stack[-1] > i:
             answer = ['NO']
             break
         elif stack[-1] == i:
             stack.pop()
             answer.append('-')
-        else:
-            while numIn != i:
-                stack.append(numIn)
-                answer.append('+')
-                numIn += 1
-            answer += ['+','-']
-            numIn += 1
-for i in answer:
-    print(i)
+            continue
+    while numIn != i:
+        stack.append(numIn)
+        answer.append('+')
+        numIn += 1
+    answer += ['+','-']
+    numIn += 1
+print('\n'.join(answer))
