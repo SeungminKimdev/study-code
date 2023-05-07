963a72c13f29ba5e331c74045b35a0754b49ff35
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-05-07 17:06:29 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 232 ms, Memory: 57880 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -2,18 +2,13 @@ import sys
 input = sys.stdin.readline
 
 pocketIn, order = map(int,input().split())
-pocketNum = []
 pocketDict = {}
 
 for i in range(1,pocketIn+1):
     tempPk = input().strip()
-    pocketNum.append(tempPk)
+    pocketDict[str(i)] = tempPk
     pocketDict[tempPk] = i
 
 for _ in range(order):
     inputO = input().strip()
-    try:
-        num = int(inputO)
-        print(pocketNum[num-1])
-    except:
-        print(pocketDict[inputO])
+    print(pocketDict[inputO])
