deb7544c0b845afca457baa1f30d5fbb548480f8
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-05-22 21:33:34 +0900

    [Silver II] Title: 색종이 만들기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -6,20 +6,18 @@ def checkBox(paper, startx, starty, n):
     if n == 1:
         paperAns[paper[startx][starty]] += 1
         return
-    check = True
+    
     fir = paper[startx][starty]
     for i in range(startx,startx+n):
         for j in range(starty,starty+n):
             if paper[i][j] != fir:
-                check = False
                 temp = n // 2
                 checkBox(paper,startx,starty,temp)
                 checkBox(paper,startx,starty+temp,temp)
                 checkBox(paper,startx+temp,starty,temp)
                 checkBox(paper,startx+temp,starty+temp,temp)
                 return
-    if check:
-        paperAns[fir] += 1
+    paperAns[fir] += 1
     return
 
 n = int(input())
