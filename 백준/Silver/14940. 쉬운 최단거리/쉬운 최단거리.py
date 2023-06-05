18cce1e3ef1ad3e504c10b0f33506e767dfcee21
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-06-05 12:23:06 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 436 ms, Memory: 39736 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -5,39 +5,36 @@ from collections import deque
 def main():
     n, m = map(int,input().split())
     mapDict = {}
-    length = []
+    length = {}
     targetX = targetY = 0
     for i in range(n):
-        mapDict[i] = []
-        length.append([0] * m)
-        inputLine = list(map(int,input().split()))
+        length[i] = [0] * m
+        mapDict[i] = list(input().strip().split())
         for j in range(m):
-            mapDict[i].append(inputLine[j])
-            if inputLine[j] == 2:
+            if mapDict[i][j] == '2':
                 targetX = i
                 targetY = j
                 
     q = deque()
     q.append((targetX,targetY))
-    moveX = [0,0,1,-1]
-    moveY = [1,-1,0,0]
+    move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     while len(q) != 0:
         nowX, nowY = q.popleft()
         nowLen = length[nowX][nowY]
-        for i in range(4):
-            nextX = nowX + moveX[i]
-            nextY = nowY + moveY[i]
-            if nextX >= 0 and nextX < n and nextY >= 0 and nextY < m and mapDict[nextX][nextY] == 1 and length[nextX][nextY] == 0:
+        for i in move:
+            nextX = nowX + i[0]
+            nextY = nowY + i[1]
+            if nextX >= 0 and nextX < n and nextY >= 0 and nextY < m and mapDict[nextX][nextY] == '1' and length[nextX][nextY] == 0:
                 length[nextX][nextY] = nowLen + 1
                 q.append((nextX,nextY))
                 
     for i in range(n):
         for j in range(m):
-            if length[i][j] == 0 and mapDict[i][j] == 1:
+            if length[i][j] == 0 and mapDict[i][j] == '1':
                 length[i][j] = -1
                 
-    for i in length:
-        print(*i,sep=' ')
+    for i in range(n):
+        print(*length[i],sep=' ')
     return
 
 if __name__ == '__main__':
