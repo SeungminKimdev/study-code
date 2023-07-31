b63f4e1a6ad8e9e519c47fb7576d8cebc18740bb
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-07-31 13:46:45 +0900

    [Silver I] Title: 정수 삼각형, Time: 116 ms, Memory: 35744 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n = int(input())
+    arr = {}
+    for i in range(n):
+        arr[i] = [*map(int,input().split())]
+    for i in range(1,n):
+        for j in range(i+1):
+            if j == 0:
+                arr[i][j] += arr[i-1][j]
+            elif j == i:
+                arr[i][j] += arr[i-1][-1]
+            else:
+                arr[i][j] += max(arr[i-1][j-1],arr[i-1][j])
+    print(max(arr[n-1]))
+    return
+
+if __name__ == '__main__':
+    main()
