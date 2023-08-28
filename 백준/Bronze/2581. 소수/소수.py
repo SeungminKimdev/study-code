0dde132f120fd43510e7acc4cc0c431a5ba6da89
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-08-28 18:04:58 +0900

    [Bronze II] Title: 소수, Time: 396 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+def main():
+    M = int(input())
+    N = int(input())
+
+    decimal = []
+    for i in range(M, N+1):
+        for j in range(2, i+1):
+            if j == i:
+                decimal.append(i)
+            if i % j == 0:
+                break
+
+    if not decimal:
+        print(-1)
+    else:
+        print(sum(decimal))
+        print(decimal[0])
+
+if __name__ == "__main__":
+    main()
