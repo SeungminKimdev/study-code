69b1f7c79776c408ecf162d8b13e190a23a5d32f
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-09-02 20:51:19 +0900

    [Bronze V] Title: 긴자리 계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+if __name__ == "__main__":
+    main()
