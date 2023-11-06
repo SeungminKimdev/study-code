1e638899850732714fc29c1dc9197b31cb925254
Seungmin <75172192+SeungminKimdev@users.noreply.github.com>
2023-11-06 14:56:06 +0900

    [Silver III] Title: 01타일, Time: 256 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -3,10 +3,13 @@ input = sys.stdin.readline
 
 if __name__ == '__main__':
     n = int(input())
-    dp = [0,1,2]
     if n < 3:
-        print(dp[n])
+        print(n)
     else:
+        a = 1
+        b = 2
         for i in range(2,n):
-            dp.append((dp[i]+dp[i-1])%15746)
-        print(dp[-1])
+            temp = (a+b) % 15746
+            a = b
+            b = temp
+        print(b)
