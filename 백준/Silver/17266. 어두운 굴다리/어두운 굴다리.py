4dd078e4787b01ac872c481e1d2714b3fa3436b1
Seungmin Kim <75172192+SeungminKimdev@users.noreply.github.com>
2025-01-13 19:06:38 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 64 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -7,7 +7,8 @@ def main():
     x = list(map(int, input().split()))
     ansList = [x[0], N-x[-1], 0] # 시작부터 가로등까지, 끝부터 가로등까지, 중간 가로등사이 거리 최대
     for i in range(0, M-1):
-        ansList[2] = max(ansList[2], sum(divmod((x[i+1]-x[i]),2)))
+        ansList[2] = max(ansList[2], x[i+1]-x[i])
+    ansList[2] = sum(divmod(ansList[2], 2))
     print(max(ansList))
 
 if __name__ == "__main__":
