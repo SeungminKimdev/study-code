71c553217a5452378c2b81b291766664ef76c8ea
Seungmin Kim <75172192+SeungminKimdev@users.noreply.github.com>
2025-01-26 20:10:27 +0900

    [Bronze IV] Title: Population, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/26561. Population/Population.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n = int(input())
+    for _ in range(n):
+        p, t = map(int, input().split())
+        print(p + t//4 - t//7)
+
+if __name__ == "__main__":
+    main()
