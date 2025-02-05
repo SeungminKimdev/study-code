d97706e409dd8b5d12ff2d3654c577b18b0d0416
Seungmin Kim <75172192+SeungminKimdev@users.noreply.github.com>
2025-02-05 11:01:34 +0900

    [Gold IV] Title: 서울에서 경산까지, Time: 92 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.py
+++ 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.py
@@ -1,23 +1,32 @@
 import sys
 input = sys.stdin.readline
 
-def main():
-    n, costs = map(int, input().split())
-    arrays = [list(map(int, input().split())) for _ in range(n)]
-    dp = [[] for _ in range(n+1)] # 모금액 합, 비용합
-    dp[0].append((0,0))
-    
-    for i in range(1, n+1):
-        for money, cost in dp[i-1]:
-            nextCost = cost + arrays[i-1][0]
-            if nextCost < costs or (nextCost == costs and i == n):
-                dp[i].append((money+arrays[i-1][1], nextCost))
-            nextCost = cost + arrays[i-1][2]
-            if nextCost < costs or (nextCost == costs and i == n):
-                dp[i].append((money+arrays[i-1][3], nextCost))
-    
-    print(max(dp[n])[0])
+def prune(state):
+    items = sorted(state.items())
+    pruned = {}
+    best = -1
+    for c, m in items:
+        if m > best:
+            pruned[c] = m
+            best = m
+    return pruned
 
+def main():
+    n, C = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    
+    dp = {0: 0}
+    for i in range(n):
+        new_dp = {}
+        for cost, money in dp.items():
+            nc = cost + arr[i][0]
+            if (i == n - 1 and nc <= C) or (i < n - 1 and nc < C):
+                new_dp[nc] = max(new_dp.get(nc, 0), money + arr[i][1])
+            nc = cost + arr[i][2]
+            if (i == n - 1 and nc <= C) or (i < n - 1 and nc < C):
+                new_dp[nc] = max(new_dp.get(nc, 0), money + arr[i][3])
+        dp = prune(new_dp)
+    print(max(dp.values()))
 
 if __name__ == "__main__":
     main()
