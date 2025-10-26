82bcba80be9d529b447376f2d7ebcfec70f9f264
Seungmin Kim <75172192+SeungminKimdev@users.noreply.github.com>
2025-10-26 21:50:10 +0900

    [level 2] Title: 타겟 넘버, Time: 171.18 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,29 @@
+from collections import deque
+
 def solution(numbers, target):
-    global answer
     answer = 0
-    def dfs(floor, num):
-        global answer
-        if floor == len(numbers):
-            if num == target:
-                answer += 1
-            return
-        dfs(floor+1,num+numbers[floor])
-        dfs(floor+1,num-numbers[floor])
-        return
-    dfs(0,0)
+    q = deque()
+    numbers_len = len(numbers)
+    for i in range(numbers_len):
+        if i == 0: # 시작 queue 만들기
+            q.append(numbers[i])
+            q.append(numbers[i]*-1)
+            continue
+        if i == numbers_len-1: # 마지막 조건 확인
+            while q:
+                number = q.popleft()
+                if number + numbers[i] == target or number - numbers[i] == target:
+                    answer += 1
+            continue
+        
+        # BFS 루트
+        q_len = len(q)
+        for _ in range(q_len):
+            number = q.popleft()
+            next_number_plus = number + numbers[i]
+            next_number_minus = number - numbers[i]
+            if abs(target - next_number_plus) <= 50 * (numbers_len - i - 1):
+                q.append(next_number_plus)
+            if abs(target - next_number_minus) <= 50 * (numbers_len - i - 1):
+                q.append(next_number_minus)
     return answer
