9772d621339f757f1eba48be2e3ea667cf7b6db0
Seungmin Kim <75172192+SeungminKimdev@users.noreply.github.com>
2025-10-28 16:05:41 +0900

    [level 2] Title: 가장 큰 수, Time: 1422.98 ms, Memory: 25.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,10 +1,5 @@
 def solution(numbers):
-    answer = ''
-    str_numbers = list(map(str,numbers))
-    str_numbers.sort(key=lambda x:x*3, reverse=True)
-    for i in str_numbers:
-        if answer == '0' and i == '0':
-            break
-        else:
-            answer += i
-    return answer
+    str_numbers = list(map(str, numbers))
+    str_numbers.sort(key=lambda x: x*3, reverse=True)
+    answer = ''.join(str_numbers)
+    return str(int(answer))
